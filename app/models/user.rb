88e204d208c884ce36d864265aca041ce731ceaf
Duy Nguyen <quangduyx188@gmail.com>
2015-10-09 14:51:33 +0700

    Add user blog

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	
+	has_many :blogs, dependent: :destroy	
 
 	attr_accessor :remember_token, :activation_token, :reset_token
   	before_save   :downcase_email
@@ -70,6 +70,11 @@ class User < ActiveRecord::Base
 	# Returns true if a password reset has expired.
 	def password_reset_expired?
 	    reset_sent_at < 2.hours.ago
+	end
+	# Defines a proto-feed.
+	# See "Following users" for the full implementation.
+	def feed
+	    Blog.where("user_id = ?", id)
 	end
   	private
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151008183607) do
+ActiveRecord::Schema.define(version: 20151009071632) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.text     "title"
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "picture"
+  end
+
+  add_index "blogs", ["user_id", "created_at"], name: "index_blogs_on_user_id_and_created_at"
+  add_index "blogs", ["user_id"], name: "index_blogs_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
