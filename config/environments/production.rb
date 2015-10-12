3451e0a3631f16d0702bc597d4d3bd04a250df42
Duy Nguyen <quangduyx188@gmail.com>
2015-10-12 16:21:01 +0700

    Sent a gamil

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
     port:                 587,
     domain:               'gmail.com',
     user_name:            'quangduyx188@gmail.com',
-    password:             'Nguyenduy93',
+    password:             'Quangduy93',
     authentication:       'plain',
     enable_starttls_auto: true
   }
