50473a57465fbbceb05c59e28236e4901848198a
Duy Nguyen <quangduyx188@gmail.com>
2015-10-12 08:09:28 +0700

    Thoi xem the nao

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,14 +18,14 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'nguyenquangduydemo.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['quangduyx188@gmail.com'],
-    :password       => ENV['Nguyenduy93'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'quangduyx188@gmail.com',
+    password:             'Nguyenduy93',
+    authentication:       'plain',
+    enable_starttls_auto: true
   }
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
